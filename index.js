cc550b3d5b3eac1fa77aa406b2fc6aeaa797fd2c
Juan <expJuan100@gmail.com>
2024-07-04 23:54:09 -0600

    mejora de la api

--- index.js
+++ index.js
@@ -28,61 +28,29 @@ app.get ('/', (req, res) => {
 });
 
 //crear una ruta para obtener todos los proyectos
-app.get ('/proyectos', (req, res) => {
+app.get ('/Biblioteca_app', (req, res) => {
     //realiza la ruta
-config.query('select * From proyectos', (err, filas) => {
+config.query('select * From biblioteca_app', (err, filas) => {
     if (err){
         console.log(err);
-        res.status(500).send('Error al obtener los proyectos');
+        res.status(500).send('Error al obtener los biblioteca_app');
 
     }else {
         res.json(filas);
     }
 
-});
-}
-)
-//crear una ruta para obtener todos los proyectos
-app.get ('/miembro', (req, res) => {
-    //realiza la ruta
-config.query('select * From miembro', (err, filas) => {
-    if (err){
-        console.log(err);
-        res.status(500).send('Error al obtener los miembro');
-
-    }else {
-        res.json(filas);
-    }
-});
-}
-)
-
-
-//crear una ruta para obtener todas las actividades de un proyecto de id en la URL
-//luego la query
-//
-app.get ('/proyecto/:id/actividades', (req, res) => {
-    //realiza la ruta
-config.query('select * From actividades where proyecto_id = ?', req.params.id,  (err, filas) => {
-    if (err){
-        console.log(err);
-        res.status(500).send('Error al obtener los miembro');
-
-    }else {
-        res.json(filas);
-    }
 });
 }
 )
 
 //Store a project in the database
-app.post('/proyecto', (req, res) => {
+app.post('/biblioteca_app', (req, res) => {
     // Get the data from the request
     const data = req.body;
     console.log(data);
 
     // Perform the query
-    config.query('INSERT INTO proyectos SET ?', data, (err, result) => {
+    config.query('INSERT INTO Biblioteca_app SET ?', data, (err, result) => {
         if (err) {
             console.log(err);
             res.status(500).send('Error al guardar el proyecto');
@@ -96,6 +64,55 @@ app.post('/proyecto', (req, res) => {
     });
 });
 
+
+app.get ('/autores', (req, res) => {
+    //realiza la ruta
+config.query('select * From autores', (err, filas) => {
+    if (err){
+        console.log(err);
+        res.status(500).send('Error al obtener los autores');
+
+    }else {
+        res.json(filas);
+    }
+});
+}
+)
+
+app.get ('/biblioteca_app/:id_autores', (req, res) => {
+    //realiza la ruta
+config.query('select * From acutores where id_autores = ?', req.params.id,  (err, filas) => {
+    if (err){
+        console.log(err);
+        res.status(500).send('Error al obtener los autores');
+
+    }else {
+        res.json(filas);
+    }
+});
+}
+)
+
+//Store a project in the database
+app.post('/Biblioteca_proyecto', (req, res) => {
+    // Get the data from the request
+    const data = req.body;
+    console.log(data);
+
+     // Perform the query
+     config.query('INSERT INTO biblioteca_app SET ?', data, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Error al guardar en la biblioteca');
+        } else {
+            // Return the response
+            res.json({
+                id: result.insertId,
+                ...data
+            });
+        }
+    });
+});
 
 //iniciar el servidor
 app.listen (port, ()  => {
